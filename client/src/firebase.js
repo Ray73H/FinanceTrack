5553e7e4fc31636687a01266e9c7492429276818
Ray Heinonen <Ray_heinonen@hotmail.com>
2024-08-11 12:37:49 -0700

    Added frontend firebase config to env. Added env to gitignore.

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbbrUpr1Sru_ay4Jh1eVSmoXdp_nG_6Lw",
-  authDomain: "finance-track-e0e22.firebaseapp.com",
-  projectId: "finance-track-e0e22",
-  storageBucket: "finance-track-e0e22.appspot.com",
-  messagingSenderId: "775013855619",
-  appId: "1:775013855619:web:754a0f2f3a2cd883c146b6",
-  measurementId: "G-Q2L1KY33XF",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
